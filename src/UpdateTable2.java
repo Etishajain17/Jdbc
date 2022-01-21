b59210b68fc87a111f2d18c365437236995b22bd
EtishaJain17 <jainetisha0714>
2022-01-21 22:22:27 +0530

    UpdateTable2 commit

---
+++ src/UpdateTable2.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class UpdateTable2
+{
+    public static void main(String[] args) throws SQLException
+    {
+        Scanner sc=new Scanner(System.in);
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical","root","");
+        Statement st=con.createStatement();
+        double increment=sc.nextDouble();
+        double salRange=sc.nextDouble();
+        String sql_query=String.format(" update employee set esal=esal+%f where esal<%f ",increment,salRange);
+        int UpdatedCount=st.executeUpdate(sql_query);
+        System.out.println(" Rows Updated : "+ UpdatedCount);
+        con.close();
+    }
+}
