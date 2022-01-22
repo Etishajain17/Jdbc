9b7aff690def237203ffa3412f029a18cf02ed49
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-22 11:55:07 +0530

    DeleteTable code commit

--- src/DeleteTable.java
+++ src/DeleteTable.java
@@ -8,8 +8,8 @@ public class DeleteTable
     public static void main(String[] args) throws SQLException
     {
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical","root","");
-        String sql_query="delete from employee where ename='Anam'";
         Statement st=con.createStatement();
+        String sql_query="delete from employee where ename='Anam'";
         int UpdatedCount=st.executeUpdate(sql_query);
         System.out.println("Rows Deleted : "+ UpdatedCount);
         con.close();
