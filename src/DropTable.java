37e93b429f264da9f8972182451171410cccce08
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-22 11:55:23 +0530

    DropTable code commit

--- src/DropTable.java
+++ src/DropTable.java
@@ -8,8 +8,8 @@ public class DropTable
     public static void main(String[] args) throws SQLException
     {
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical","root","");
-        String sql_query="drop table employee";
         Statement st=con.createStatement();
+        String sql_query="drop table employee";
         st.executeUpdate(sql_query);
         System.out.println("Table Deleted Successfully");
         con.close();
