8284e570489df0cafba4d0e22712b74e4bd0b847
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-22 12:58:06 +0530

    AggregateTable1 code commit

---
+++ src/AggregateTable1.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class AggregateTable1
+{
+    public static void main(String[] args) throws SQLException
+    {
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical","root","");
+        Statement st=con.createStatement();
+        String sql_query="select count(*) from employee";
+        ResultSet rs = st.executeQuery(sql_query);
+        if(rs.next())
+        {
+            System.out.println("Total number of rows in Table : "+rs.getInt(1));
+        }
+        System.out.println("...............................");
+        con.close();
+    }
+}
