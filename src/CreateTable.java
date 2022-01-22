4654977f42343299dc157b7820a71f73554bd60a
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-22 11:54:48 +0530

    CreateTable code commit

--- src/CreateTable.java
+++ src/CreateTable.java
@@ -5,8 +5,8 @@ public class CreateTable
     public static void main(String[] args) throws ClassNotFoundException, SQLException
     {
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical","root","");
-        String sql_query="insert into employee values(1001,'Etisha',5000,'Mathura')";
         Statement st=con.createStatement();
+        String sql_query="insert into employee values(1001,'Etisha',5000,'Mathura')";
         st.executeUpdate(sql_query);
         System.out.println("Values inserted Successfully");
         con.close();
