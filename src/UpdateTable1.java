fc9ae8d610a00422258d4d2ee5b277dfb34968f4
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-22 11:56:25 +0530

    UpdateTable1 code commit

--- src/UpdateTable1.java
+++ src/UpdateTable1.java
@@ -8,8 +8,8 @@ public class UpdateTable1
     public static void main(String[] args) throws SQLException
     {
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical","root","");
-        String sql_query="update employee set esal=7777.5 where ename='Etisha'";
         Statement st=con.createStatement();
+        String sql_query="update employee set esal=7777.5 where ename='Etisha'";
         int UpdatedCount=st.executeUpdate(sql_query);
         System.out.println("Rows Updated : "+ UpdatedCount);
         con.close();
