bf32b8cf6b2603e4d27dca6af269ce9f6b5c350d
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-22 11:55:44 +0530

    InsertTable1 code commit

--- src/InsertTable1.java
+++ src/InsertTable1.java
@@ -10,12 +10,12 @@ public class InsertTable1
     {
         Scanner sc=new Scanner(System.in);
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical","root","");
+        Statement st=con.createStatement();
         int eno=sc.nextInt();
         String ename=sc.next();
         float esal=sc.nextFloat();
         String eaddr=sc.next();
         String sql_query="insert into employee values("+eno+",'"+ename+"',"+esal+",'"+eaddr+"')";
-        Statement st=con.createStatement();
         st.executeUpdate(sql_query);
         System.out.println("Values Inserted Successfully");
         con.close();
