69496f1969a4a1ad68d6b5fd4b6d7fcdbf9d7e12
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-23 23:57:34 +0530

    Batch used for executing multiple statements together

---
+++ src/_025_BatchWithStatement.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class _025_BatchWithStatement
+{
+    public static void main(String[] args) throws SQLException
+    {
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical","root","");
+        Statement st=con.createStatement();
+        st.addBatch("insert into employee values(1002,'Manvi',1230.67,'Punjab')");
+        st.addBatch("update employee set esal=esal+700 where esal<6000");
+        st.addBatch("delete from employee where esal>10000");
+        int[] counter=st.executeBatch();
+        int updateCount=0;
+        for(int x: counter)
+        {
+            updateCount=updateCount+x;
+        }
+        System.out.println("Number Of rows updated:"+updateCount);
+        con.close();
+    }
+}
