2a10375c1917d29553749f53a8a694931e6bccf3
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-24 14:54:05 +0530

    Image(BLOB) Type Data in database

---
+++ src/_027_BLOBTypeInsert.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class _027_BLOBTypeInsert
+{
+    public static void main(String[] args) throws SQLException, FileNotFoundException
+    {
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/people","root","");
+        PreparedStatement ps=con.prepareStatement("insert into persons values(?,?)");
+        ps.setString(1,"Etisha");
+        File fi=new File("C:\\Users\\Etisha Jain\\Desktop\\pic.jpeg");
+        FileInputStream fis=new FileInputStream(fi);
+        ps.setBinaryStream(2,fis);
+        System.out.println("Inserting image from :"+fi.getAbsolutePath());
+        int updateCount =ps.executeUpdate();
+        if(updateCount==1)
+        {
+            System.out.println("Record Inserted");
+        }
+        else
+        {
+            System.out.println("Record not Inserted");
+        }
+    }
+}
