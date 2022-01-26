6aed00308f4174a0100c5c05b02c1c5257ddc5c1
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-27 00:19:17 +0530

    Meta Data--->ParameterMetaData(about Parameter)

--- src/_045_ParameterMetaData1.java
+++ src/_045_ParameterMetaData1.java
@@ -7,19 +7,18 @@ public class _045_ParameterMetaData1
      */
     public static void main(String[] args) throws SQLException
     {
-        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical?user=root&password=");
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical?generateSimpleParameterMetadata=true&user=root&password=");
         PreparedStatement ps=con.prepareStatement("insert into employee values(?,?,?,?)");
         ParameterMetaData pmd=ps.getParameterMetaData();
         int count=pmd.getParameterCount();
         for (int i = 1; i <= count ; i++)
         {
             System.out.println("Parameter Number: "+i);
-//            System.out.println("Parameter Mode: "+pmd.getParameterMode(i));
-//            System.out.println("Parameter Type: "+pmd.getParameterType(i));
-//            System.out.println("Parameter Precision: "+pmd.getPrecision(i));
-//            System.out.println("Parameter Scale: "+pmd.getScale(i));
-//            System.out.println("Parameter isSigned: "+pmd.isSigned(i));
-//            System.out.println("Parameter isNullable: "+pmd.isNullable(i));
+            System.out.println("Parameter Mode: "+pmd.getParameterMode(i));
+            System.out.println("Parameter Type: "+pmd.getParameterType(i));
+            System.out.println("Parameter Precision: "+pmd.getPrecision(i));
+            System.out.println("Parameter Scale: "+pmd.getScale(i));
+            System.out.println("Parameter isSigned: "+pmd.isSigned(i));
             System.out.println("...........................");
         }
         con.close();
