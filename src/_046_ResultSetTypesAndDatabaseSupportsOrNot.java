ac1a822bc358f321be1e4a8e2f4ba2f0b71f3971
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-27 16:06:59 +0530

    Types of Resultsets and check whether database supports or not

---
+++ src/_046_ResultSetTypesAndDatabaseSupportsOrNot.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+
+public class _046_ResultSetTypesAndDatabaseSupportsOrNot
+{
+    /*
+    Division-1:Based on operations performed
+    - Read Only ResultSets(Static ResultSets)   ---->CONCUR_READ_ONLY ->1007
+    - Updatable ResultSets(Dynamic ResultSets)  ---->CONCUR_UPDATABLE ->1008
+
+    Division-2:Based on Cursor Movement
+    - Forward Only(Non-Scrollable) ResultSet    ---->TYPE_FORWARD_ONLY ->1003
+    - Scrollable ResultSets
+          - Scroll Insensitive ResultSet        ---->TYPE_SCROLL_INSENSITIVE ->1004
+          - Scroll Sensitive ResultSet          ---->TYPE_SCROLL_SENSITIVE   ->1005
+     */
+    public static void main(String[] args) throws Exception
+    {
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical?user=root&password=");
+        DatabaseMetaData dbmd=con.getMetaData();
+        System.out.println(dbmd.supportsResultSetConcurrency(1003,1007));
+        System.out.println(dbmd.supportsResultSetConcurrency(1003,1008));
+        System.out.println(dbmd.supportsResultSetType(1003));
+        System.out.println(dbmd.supportsResultSetType(1004));
+        System.out.println(dbmd.supportsResultSetType(1005));
+        con.close();
+    }
+}
