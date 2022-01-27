e1faeccea3970008c7685f62dec782cdc43cc41a
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-27 16:10:28 +0530

    Types of Resultsets and check whether database supports or not

--- src/_046_ResultSetTypesAndDatabaseSupportsOrNot.java
+++ src/_046_ResultSetTypesAndDatabaseSupportsOrNot.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 public class _046_ResultSetTypesAndDatabaseSupportsOrNot
 {
     /*
-    Division-1:Based on operations performed
+    Division-1:Based on operations performed(allowed values for mode)
     - Read Only ResultSets(Static ResultSets)   ---->CONCUR_READ_ONLY ->1007
     - Updatable ResultSets(Dynamic ResultSets)  ---->CONCUR_UPDATABLE ->1008
 
-    Division-2:Based on Cursor Movement
+    Division-2:Based on Cursor Movement(allowed values for type)
     - Forward Only(Non-Scrollable) ResultSet    ---->TYPE_FORWARD_ONLY ->1003
     - Scrollable ResultSets
           - Scroll Insensitive ResultSet        ---->TYPE_SCROLL_INSENSITIVE ->1004
